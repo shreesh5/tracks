c2005637553dcdf29baee1ce2ff433e9ac7cc88b
Shreesh Nayak <shreesh@pop-os.localdomain>
2020-07-18 11:52:42 -0700

    added auth context and api

---
+++ src/context/AuthContext.js
@@ -0,0 +1,38 @@
+import createDataContext from './createDataContext';
+
+const authReducer = (state, action) => {
+    switch (action.type) {
+        default: 
+            return state;
+    }
+};
+
+const signup = (dispatch) => {
+    return ({ email, password }) => {
+        // make api request to sign up with that email and password
+
+        // if we sign up, modify our state, and say that we are authenticated
+
+        // if signing up fails, we probably need to reflect an error message somewhere
+    };
+};
+
+const signin = (dispatch) => {
+    return ({ email, password}) => {
+        // try to signin
+        // handle success by updating state
+        // handle failure by showing error message (somehow)
+    }
+}
+
+const signout = (dispatch) => {
+    return () => {
+        // somehow sign out
+    }
+}
+
+export const { Context, Provider } = createDataContext(
+    authReducer, 
+    { },
+    { isSignedIn: false }
+);
