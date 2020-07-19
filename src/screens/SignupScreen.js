0c392e19ec748d1ae30f685b59d7dc84ace38467
Shreesh Nayak <shreesh@pop-os.localdomain>
2020-07-18 19:57:23 -0700

    added navlink component

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,10 +1,8 @@
 import React, { useContext } from 'react';
-import { StyleSheet, View, TouchableOpacity } from 'react-native';
-import { Text } from 'react-native-elements';
-
-import Spacer from '../components/Spacer';
+import { StyleSheet, View } from 'react-native';
 import { Context as AuthContext } from '../context/AuthContext';
 import AuthForm from '../components/AuthForm';
+import NavLink from '../components/NavLink';
 
 const SignupScreen = ({ navigation }) => {
     
@@ -19,11 +17,10 @@ const SignupScreen = ({ navigation }) => {
                 // onSubmit={({ email, password}) => signup({ email, password})}
                 onSubmit={signup}
             />
-            <TouchableOpacity onPress={() => navigation.navigate('Signin')}>
-                <Spacer>
-                    <Text style={styles.link}>Already have an account? Sign in instead.</Text>
-                </Spacer>
-            </TouchableOpacity>
+            <NavLink 
+                routeName="Signin"
+                text="Already have an account? Sign in instead."
+            />
         </View>
     );
 }
@@ -40,9 +37,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         marginBottom: 200
     },
-    link: {
-        color: 'blue'
-    }
+
 });
 
 export default SignupScreen;
