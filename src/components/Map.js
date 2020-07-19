27059df252bd945eb6cb40fdc4975d666cbb141e
Shreesh Nayak <shreeshnayak@gmail.com>
2020-07-19 11:02:27 -0700

    added coordinates

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Polyline } from 'react-native-maps';
 
 const Map = () => {
+
+    let points = [];
+    for (let i=0; i<20; i++) {
+        points.push({
+            latitude: 37.33233 + i * 0.001,
+            longitude: -122.03121 + i * 0.01
+        })
+    }
+
     return (
         <MapView 
             style={styles.map}
@@ -12,7 +21,11 @@ const Map = () => {
                 latitudeDelta: 0.01,
                 longitudeDelta: 0.01,
             }}
-        />
+        >
+            <Polyline 
+                coordinates={points}
+            />
+        </MapView>
     );
 };
 
