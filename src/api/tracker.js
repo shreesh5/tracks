067221b41f38c545d7bf67c3ccb5a18667086167
Shreesh Nayak <shreeshnayak@gmail.com>
2020-07-19 17:31:53 -0700

    Added request for location permissions

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://7efa52c0effa.ngrok.io'
+    baseURL: 'http://1915ddbdf615.ngrok.io'
 });
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import { SafeAreaView } from 'react-navigation';
+import { requestPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location';
 import Map from '../components/Map';
+import '../_mockLocation';
 
 const TrackCreateScreen = () => {
+
+    const [err, setErr] = useState(null);
+
+    const startWatching = async () => {
+        try {
+            const { granted } = await requestPermissionsAsync();
+            if (!granted) {
+                throw new Error('Location permission not granted');
+            }
+            await watchPositionAsync({
+                accuracy: Accuracy.BestForNavigation,
+                timeInterval: 1000,
+                distanceInterval: 10
+            }, (location) => {
+                console.log(location);
+            });
+        } catch (e) {
+            setErr(e);
+        }
+    }
+
+    useEffect(() => {
+        startWatching();
+    }, []);
+
     return (
         <SafeAreaView forceInset={{ top: 'always' }}>
             <Text h2>Create a Track</Text>
             <Map />
+            {err ? <Text>Please enable location services</Text> : null }
         </SafeAreaView>
     );
 };
@@ -16,3 +44,16 @@ const TrackCreateScreen = () => {
 const styles = StyleSheet.create({});
 
 export default TrackCreateScreen;
+
+/*
+  const startWatching = async () => {
+    try {
+      const { granted } = await requestPermissionsAsync();
+      if (!granted) {
+        throw new Error('Location permission not granted');
+      }
+    } catch (e) {
+      setErr(e);
+    }
+  };
+*/
